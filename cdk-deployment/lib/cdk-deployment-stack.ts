0aaa071b3ad4d60c4c192e9644da5a9efe9c0455
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-06-06 22:13:32 -0400

    setup cdk for s3 bucket and cloudfront

---
+++ cdk-deployment/lib/cdk-deployment-stack.ts
@@ -0,0 +1,61 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
+
+export class CdkDeploymentStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, 'JSCC-OAI');
+
+    const siteBucket = new s3.Bucket(this, 'MyStaticBucket', {
+      bucketName: 'elena-my-shop-s3',
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [siteBucket.arnForObjects('*')],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      'MyStatic-distribution',
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+              originAccessIdentity: cloudFrontOAI,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+    new s3deploy.BucketDeployment(this, 'MyBucket-Deployment', {
+      sources: [s3deploy.Source.asset(path.resolve(__dirname, '../../dist'))],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+    new cdk.CfnOutput(this, 'DistributionURL', {
+      value: distribution.distributionDomainName,
+      description: 'URL of the CloudFront distribution',
+    });
+  }
+}
