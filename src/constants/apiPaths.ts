6b64a6e559b262b4dc718bdb4d26acae569f6ed6
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-06-30 00:46:45 -0400

    add get and put request

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
   order: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
-  import: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
+  import: "https://1uzy0g2wq8.execute-api.ca-central-1.amazonaws.com/prod",
   bff: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
   cart: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
 };
