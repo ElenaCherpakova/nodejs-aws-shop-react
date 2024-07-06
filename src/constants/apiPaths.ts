3124f3458179db8291f66930022fe91ecc380b33
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-07-06 15:51:01 -0400

    adjust apiPath

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
-  order: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
-  import: "https://1uzy0g2wq8.execute-api.ca-central-1.amazonaws.com/prod",
-  bff: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
-  cart: "https://3oxl5ov7o4.execute-api.ca-central-1.amazonaws.com/prod",
+  product: "https://1lb8wjk14a.execute-api.ca-central-1.amazonaws.com/prod",
+  order: "https://1lb8wjk14a.execute-api.ca-central-1.amazonaws.com/prod",
+  import: "https://x62xouq253.execute-api.ca-central-1.amazonaws.com/prod",
+  bff: "https://1lb8wjk14a.execute-api.ca-central-1.amazonaws.com/prod",
+  cart: "https://1lb8wjk14a.execute-api.ca-central-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
