19f82c3f5f44488874a2da60459049e659e07895
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-07-11 01:49:05 -0400

    feat:create context and handle authorization on the global level in index.tsx

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import axios from "axios";
+import { useAlert } from "~/components/AlertContext";
 
 type CSVFileImportProps = {
   url: string;
@@ -11,40 +12,59 @@ type CSVFileImportProps = {
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File | "">();
 
+  const setAlert = useAlert();
+
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
       const file = files[0];
       setFile(file);
+      setAlert(null);
     }
   };
 
   const removeFile = () => {
     setFile(undefined);
+    setAlert(null);
   };
 
   const uploadFile = async () => {
-    console.log("uploadFile to", url);
-
     // Get the presigned URL
     if (file) {
-      const response = await axios.get(url, {
-        params: {
-          name: encodeURIComponent(file.name),
-        },
-      });
-      console.log("File to upload: ", file.name);
-      console.log("preSignedUrl ", response.data.url);
-      const preSignedUrl = response.data.url;
-      const result = await fetch(preSignedUrl, {
-        method: "PUT",
-        body: file,
-        headers: {
-          "Content-Type": "text/csv",
-        },
-      });
-      console.log("Result: ", result);
-      setFile("");
+      const authorization = localStorage.getItem("authorization_token");
+      if (!authorization) {
+        return setAlert({
+          type: "error",
+          text: "Unauthorized: Please check your credentials.",
+        });
+      }
+      try {
+        const { data } = await axios.get(url, {
+          headers: {
+            Authorization: `Basic ${authorization}`,
+          },
+          params: {
+            name: encodeURIComponent(file.name),
+          },
+        });
+
+        const preSignedUrl = data.url;
+        const result = await fetch(preSignedUrl, {
+          method: "PUT",
+          body: file,
+          headers: {
+            "Content-Type": "text/csv",
+          },
+        });
+        if (!result.ok) {
+          throw new Error("File upload failed");
+        }
+
+        setFile("");
+        setAlert({ type: "success", text: "File uploaded successfully" });
+      } catch (error) {
+        setAlert({ type: "error", text: "File upload failed" });
+      }
     }
   };
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import App from "~/components/App/App";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -7,6 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import { AlertProvider, useAlert } from "./components/AlertContext";
+import axios from "axios";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +21,44 @@ if (import.meta.env.DEV) {
   worker.start({ onUnhandledRequest: "bypass" });
 }
 
+const AppWrapper: React.FC = () => {
+  const setAlert = useAlert();
+
+  useEffect(() => {
+    const interceptor = axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        console.log("Interceptor caught an error:", (error as Error).message);
+        if (axios.isAxiosError(error) && error.response) {
+          const { status } = error.response;
+          setAlert(
+            status === 401
+              ? {
+                  type: "error",
+                  text: "Unauthorized: Please check your credentials.",
+                }
+              : status === 403
+              ? {
+                  type: "error",
+                  text: "Forbidden: You do not have permission to perform this action.",
+                }
+              : { type: "error", text: "Upload failed" }
+          );
+        } else {
+          setAlert({ type: "error", text: "An unexpected error occurred" });
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axios.interceptors.response.eject(interceptor);
+    };
+  }, [setAlert]);
+
+  return <App />;
+};
+
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
@@ -28,7 +68,9 @@ root.render(
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <App />
+          <AlertProvider>
+            <AppWrapper />
+          </AlertProvider>
         </ThemeProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
